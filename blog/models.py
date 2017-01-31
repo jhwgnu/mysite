91aade1fa9dfed2c33340a35da44cd4f9bc03b0d
jhwgnu <jhwon11@snu.ac.kr>
2017-01-31 15:35:36 +0900

    admin 페이지에서 Post 추가할 때 페이지 설정

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    title = models.CharField(max_length=100, verbose_name = '제목', help_text = '제목을 입력하세요')
+    content = models.TextField(verbose_name = '내용', help_text = '내용을 입력하세요')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
