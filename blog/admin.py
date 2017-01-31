f8528b2b14932cb081f9007813cdee02ec2246dd
jhwgnu <jhwon11@snu.ac.kr>
2017-01-31 15:47:48 +0900

    admin의 Post 페이지 꾸미기

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from blog.models import Post
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    def content_length(self, post):
+        return '{}'.format(len(post.content))
+
+    content_length.short_description = '글자수'
+
+    list_display = ['id', 'title', 'content_length', 'created_at']
+    list_display_links = ['title']
+    list_filter = ['created_at']
+    search_fields = ['title']
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class Post(models.Model):
     content = models.TextField(verbose_name = '내용', help_text = '내용을 입력하세요')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
