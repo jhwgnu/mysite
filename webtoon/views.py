dd4f2309f478292b1dcfbe6e59a1636da3fbae68
jhwgnu <jhwon11@snu.ac.kr>
2017-02-02 10:37:39 +0900

    웹툰 리스트 검색 구현

--- webtoon/views.py
+++ webtoon/views.py
@@ -3,8 +3,18 @@ from .models import Webtoon
 
 
 def webtoon_list(request):
+    # return render(request, 'webtoon/webtoon_list.html', {
+    #     'webtoon_list':Webtoon.objects.all(),
+    # })
+    q = request.GET.get('q', '')
+    qs = Webtoon.objects.all()
+
+    if q:
+        qs = qs.filter(title__icontains=q)
+
     return render(request, 'webtoon/webtoon_list.html', {
-        'webtoon_list':Webtoon.objects.all(),
+        'webtoon_list':qs,
+        'webtoon':q,
     })
 
 
