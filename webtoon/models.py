6f9cf916307763137745693981583d7cecc417c9
jhwgnu <jhwon11@snu.ac.kr>
2017-02-02 11:34:45 +0900

    Webtoon 모델에 crawl_naver() 클래스함수 추가

--- webtoon/models.py
+++ webtoon/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import requests
+from bs4 import BeautifulSoup
 
 class Webtoon(models.Model):
     title = models.CharField(max_length=100, verbose_name = '제목', help_text = '제목을 입력하세요')
@@ -7,3 +9,18 @@ class Webtoon(models.Model):
 
     def __str__(self):
         return self.title
+
+    @classmethod
+    def crawl_naver(cls): # cls == Webtoon
+        url = "http://comic.naver.com/webtoon/weekday.nhn"
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        for a_tag in soup.select('.daily_all li a'):
+            try:
+                title = a_tag['title']
+                cls.objects.create(title=title)
+                print('created', title)
+            except KeyError:
+                pass
+    # Webtoon.crawl_naver()
