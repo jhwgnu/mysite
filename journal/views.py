39ae75d7cd61c1ea0e120186777466465c877aaf
jhwgnu <jhwon11@snu.ac.kr>
2017-02-07 14:58:50 +0900

    저널 앱 생성

--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from blog.models import Post
+# from django.http import HttpResponse
 
-# Create your views here.
+# def post_list(request):
+#     # return HttpResponse("리스트 만들기")
+#     return render(request, 'blog/post_list.html', {
+#         'post_list': Post.objects.all(),
+#     })
+#
+# def post_detail(request, pk):
+#     post = Post.objects.get(id=pk)
+#     return render(request, 'blog/post_detail.html', {
+#         'post': post,
+#     })
+#
+# def post_new(request):
+#     print(request.GET)
+#     print(request.POST)
+#     print(request.FILES)
+#     return render(request, 'blog/post_form.html')
