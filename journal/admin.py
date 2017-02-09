70d38373961666662e059eaee986db4c4d5ef39b
jhwgnu <jhwon11@snu.ac.kr>
2017-02-09 10:20:18 +0900

    오타 수정 / db에 새 모델 반영

--- journal/admin.py
+++ journal/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from journal.models import Post
 
 # Register your models here.
 
--- journal/models.py
+++ journal/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    created_at = models.DataTimeField(auto_now_add=True)
-    created_at = models.DataTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
         return reverse('journal:post_detail', args=[self.pk])
