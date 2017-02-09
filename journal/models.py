10976679291ad2b518f03e791ce154bfa3a4d1af
jhwgnu <jhwon11@snu.ac.kr>
2017-02-09 10:16:20 +0900

    journal 모델, 폼 구현

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DataTimeField(auto_now_add=True)
+    created_at = models.DataTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('journal:post_detail', args=[self.pk])
